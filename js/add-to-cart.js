207f4f7cea82c005e7249a1e6b48ea10dde3c93b
Lief Swanson <liefs@sfu.ca>
2018-10-24 19:50:42 -0700

    add shopping cart button when something in cart

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -2,17 +2,26 @@
 
 console.log("js loaded");
 
+var checkoutCart = document.querySelector('#cart');
+
 var button = document.querySelector(".add-to-cart");
+
 button.addEventListener("click",
     function() {
         if (button.classList.contains("add-to-cart")) {
             button.classList.add("remove-from-cart");
             button.classList.remove("add-to-cart");
             button.textContent = "Remove from cart";
+
+            checkoutCart.classList.remove('hidden');
+            checkoutCart.setAttribute('aria-hidden', 'false');
             return;
         }
         button.classList.add("add-to-cart");
         button.classList.remove("remove-from-cart");
         button.textContent = "Add to cart";
+
+        checkoutCart.classList.add('hidden');
+        checkoutCart.setAttribute('aria-hidden', 'true');
     }
 );
